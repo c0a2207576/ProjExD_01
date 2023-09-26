06683a389407d7ffe964a495ca023af55c611759
naruju <c0a2207576@edu.teu.ac.jp>
2023-09-26 17:11:22 +0900

    コミット1

--- pygame_image.py
+++ pygame_image.py
@@ -7,14 +7,14 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01-20230926/fig/pg_bg.jpg")
-    kk_img = pg.image.load("ex01-20230926/fig/3.png")
+    #kk_img = pg.image.load("ex01-20230926/fig/3.png")
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img,[300,200])
+        #screen.blit(kk_img,[300,200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
