c1253544eeea656f4457b0b86b6b3f853da526ca
naruju <c0a2207576@edu.teu.ac.jp>
2023-09-26 17:15:47 +0900

    コミット4

--- pygame_image.py
+++ pygame_image.py
@@ -14,15 +14,14 @@ def main():
     tmr = 0
     while True:
         for event in pg.event.get():
-            if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img,[300,200])
+            if event.type == pg.QUIT: return       
+        x=tmr%1600
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img, [1600-x, 0])
+        screen.blit(kk_imgs[tmr%2], [300, 200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
-
-
 if __name__ == "__main__":
     pg.init()
     main()
